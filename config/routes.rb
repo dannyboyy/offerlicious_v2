9b09b5b9726522abf82710fdea09b71b6ddf1505
Danny Wu <wushuo1988@hotmail.com>
2015-08-02 21:59:37 -0400

    use acts_as_votable gem to develop voting functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :questions
 
   root 'questions#index'
+
+  resources :questions do
+    member do
+      put "like", to: "questions#upvote"
+      put "dislike", to: "questions#downvote"
+    end
+  end
+
+
 end
